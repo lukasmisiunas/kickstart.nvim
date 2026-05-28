c5faabebc7930f2a53cbf1330aac6ca9b8d1edba
lukasmisiunas <lukas.misiunas1@gmail.com>
2026-05-28 20:10:18 +0300

    Exclude jsonc

--- init.lua
+++ init.lua
@@ -910,7 +910,7 @@ do
   vim.pack.add { { src = gh 'nvim-treesitter/nvim-treesitter', version = 'main' } }
 
   -- Ensure basic parsers are installed
-  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'javascript', 'typescript', 'tsx', 'json', 'jsonc' }
+  local parsers = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'javascript', 'typescript', 'tsx', 'json' }
   require('nvim-treesitter').install(parsers)
 
   ---@param buf integer
