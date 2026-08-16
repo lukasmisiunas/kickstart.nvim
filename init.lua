815d61f898759079c3e1abcc6bbc24b6cdacd240
lukasmisiunas <lukas.misiunas1@gmail.com>
2026-08-17 02:29:07 +0300

    Enable rust_analyzer and rustfmt

--- init.lua
+++ init.lua
@@ -891,7 +891,7 @@ do
     -- clangd = {},
     -- gopls = {},
     -- pyright = {},
-    -- rust_analyzer = {},
+    rust_analyzer = {},
     --
     -- Some languages (like typescript) have entire language plugins that can be useful:
     --    https://github.com/pmizio/typescript-tools.nvim
@@ -1005,7 +1005,7 @@ do
     },
     -- You can also specify external formatters in here.
     formatters_by_ft = {
-      -- rust = { 'rustfmt' },
+      rust = { 'rustfmt' },
       -- Conform can also run multiple formatters sequentially
       -- python = { "isort", "black" },
       --
